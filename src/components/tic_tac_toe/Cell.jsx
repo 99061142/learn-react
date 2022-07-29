a90d99a23b7be25c31c17fc114be5004fe3b50b8
99061142 <99061142@mydavinci.nl>
2022-07-30 01:34:01 +0200

    Moved cell to specific folder + changed file name

--- src/components/tic_tac_toe/Cell.jsx
+++ src/components/tic_tac_toe/Cell.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class TicTacToeCell extends Component {    
+export default class Cell extends Component {    
     render() {
     const styling = {
         border: "1px solid black",
