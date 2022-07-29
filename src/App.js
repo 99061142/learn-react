f9dad5fea73cd82c4947ad617133bf6e19abfb45
99061142 <99061142@mydavinci.nl>
2022-07-30 01:28:46 +0200

    Call tic tac toe board

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Header from './components/Header';
 import Counter from './components/Counter';
 import Footer from './components/Footer';
+import TicTacToeBoard from './components/TicTacToeBoard';
 
 const App = () => {
     return (
         <div className="container">
-            <Header/>
-            <Counter />
-            <Footer />
+            <TicTacToeBoard />
         </div>
     );
 }
